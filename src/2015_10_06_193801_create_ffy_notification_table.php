b97b427a2161f965042511c8bf182a3b2c23e0a5
Tasos Protopapas <tprotopgr@gmail.com>
2015-10-20 11:09:36 +0300

    migration

---
+++ src/2015_10_06_193801_create_ffy_notification_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFfyNotificationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ffy_notifications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('notification_type');
+            $table->boolean('seen')->default(0);
+            $table->string('data')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('ffy_notifications');
+    }
+}
