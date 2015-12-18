9970b5ad30aea75a7a1bcf65328e412fa5b7929e
Tasos Protopapas <tprotopgr@gmail.com>
2015-12-19 00:07:58 +0200

    test

--- views/index.blade.php
+++ views/index.blade.php
@@ -1,6 +1,6 @@
 <h2>Notification list</h2>
 <ul class="list-unstyled">
     @foreach($notifications as $notification)
-        <li>{{$notification->notification_type}}</li>
+        <li>{{$notification->notification_type}} // {{Auth::user($notification->user_id) }}</li>
     @endforeach
 </ul>
