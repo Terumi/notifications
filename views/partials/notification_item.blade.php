a5724f5c5f8184f109f939a5f1a334c5b31a96c7
Tasos Protopapas <tprotopgr@gmail.com>
2015-12-21 17:52:10 +0200

    follow_links

--- views/partials/notification_item.blade.php
+++ views/partials/notification_item.blade.php
@@ -1,8 +1,12 @@
 <li>
     @if(!$notification->seen)
-    <a href="{{url('notifications/see', ['id' => $notification->id])}}">mark seen</a>
+        <a href="{{url('notifications/see', ['id' => $notification->id])}}">mark seen</a>
     @else
-    <a href="{{url('notifications/unsee', ['id' => $notification->id])}}">mark unseen</a>
+        <a href="{{url('notifications/unsee', ['id' => $notification->id])}}">mark unseen</a>
     @endif
+    {{--notification description--}}
     {{Auth::user($notification->user_id)->email }} // id: {{$notification->notification_type}} // seen: {{$notification->seen}}
+
+    {{--follow link--}}
+    <a href="{{url('notifications/follow', ['id' => $notification->id])}}" target="_blank">follow</a>
 </li>
