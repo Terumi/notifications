e4688f208c5ff0e1527bb24ff0e4902362436c4e
Tasos Protopapas <tprotopgr@gmail.com>
2015-12-21 15:40:45 +0200

    routes

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,3 +1,22 @@
 <?php
 
 Route::get('notifications', 'ffy\notifications\Http\Controllers\NotificationController@index');
+Route::get('notifications/create', function () {
+    echo 'creating notification';
+    $notification = Notifier::notify(1, 1, ['name' => 'Nikos', 'val' => rand(1, 1000)]);
+
+    $seen = rand(0, 1);
+    if($seen == 1){
+        $notification->seen = 1;
+        $notification->seen_at = Carbon::today()->subDays(rand(1, 400));
+        $notification->save();
+    }
+    echo "<br>done!";
+});
+
+Route::get('notifications/view', function () {
+    Auth::loginUsingId(1);
+    $notifications = Auth::user()->notifications;
+    //this returns the package view
+    return view('notifications::index')->with('notifications', $notifications);
+});
